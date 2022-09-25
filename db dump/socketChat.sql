52c8e97cc3ef41dca74156e62aa031398dabd595
Cesar Peralta <cgpc1985@gmail.com>
2022-09-25 09:54:06 -0400

    Db dump

---
+++ db dump/socketChat.sql
@@ -0,0 +1,57 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.22-MariaDB : Database - node2
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`node2` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `sochetChat`;
+
+/*Table structure for table `salas` */
+
+DROP TABLE IF EXISTS `salas`;
+
+CREATE TABLE `salas` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255) NOT NULL,
+  `active` INT(11) NOT NULL DEFAULT 0,
+  `createdAt` TIMESTAMP NULL DEFAULT NULL,
+  `updatedAt` TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `salas` */
+
+insert  into `salas`(`id`,`nombre`,`active`,`createdAt`,`updatedAt`) values (1,'Amigos',1,NULL,NULL),(2,'Fútbol',1,NULL,NULL),(3,'Videojuegos',1,NULL,NULL);
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`nombre`,`password`,`email`,`createdAt`,`updatedAt`) values (1,'Cesar','$2a$10$Dpq5xanr/99XPttQUgjin.Ai5n1q2Z09dJFU5YeyaCHKfs0jL/5Gy','cesar@gmail.com',NULL,NULL),(2,'Carlos','$2a$10$iD4llR6Nw2CBei9D74MN2uowhJDVJQG031z3wc5p2wGmxzwQ2FC1S','carlos@gmail.com',NULL,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
