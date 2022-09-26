b315ae24f4080a5eb511132254db550811266277
Cesar Peralta <cgpc1985@gmail.com>
2022-09-26 09:06:03 -0400

    Added db dump

--- db dump/socketChat.sql
+++ db dump/socketChat.sql
@@ -14,20 +14,20 @@ MySQL - 5.5.5-10.4.22-MariaDB : Database - node2
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`node2` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-USE `sochetChat`;
+USE `node2`;
 
 /*Table structure for table `salas` */
 
 DROP TABLE IF EXISTS `salas`;
 
 CREATE TABLE `salas` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(255) NOT NULL,
-  `active` INT(11) NOT NULL DEFAULT 0,
-  `createdAt` TIMESTAMP NULL DEFAULT NULL,
-  `updatedAt` TIMESTAMP NULL DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `active` int(11) NOT NULL DEFAULT 0,
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=INNODB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `salas` */
 
@@ -42,6 +42,7 @@ CREATE TABLE `users` (
   `nombre` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
+  `image` varchar(255) NOT NULL,
   `createdAt` timestamp NULL DEFAULT NULL,
   `updatedAt` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -49,7 +50,7 @@ CREATE TABLE `users` (
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`nombre`,`password`,`email`,`createdAt`,`updatedAt`) values (1,'Cesar','$2a$10$Dpq5xanr/99XPttQUgjin.Ai5n1q2Z09dJFU5YeyaCHKfs0jL/5Gy','cesar@gmail.com',NULL,NULL),(2,'Carlos','$2a$10$iD4llR6Nw2CBei9D74MN2uowhJDVJQG031z3wc5p2wGmxzwQ2FC1S','carlos@gmail.com',NULL,NULL);
+insert  into `users`(`id`,`nombre`,`password`,`email`,`image`,`createdAt`,`updatedAt`) values (1,'Cesar','$2a$10$ipEuxdu4lfCmmrYxTx0uh.JLq3luLa8jC97WoJXvMXPVHAbOld4gy','cesar@gmail.com','b084de7d-66c3-4232-b592-756529e83edb.jpg',NULL,'2022-09-26 08:59:16'),(2,'Carlos','$2a$10$9UeKh.XlV8DCCvUjM2x8A.QVFX/uDlaCYDOHEy.BJbAuZv.8Crcde','carlos@gmail.com','',NULL,'2022-09-26 08:59:32');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
